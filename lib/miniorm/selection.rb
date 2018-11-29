ebe31398f32d35da19a6a8bcdd9ecd8421e0277a
Lassiter Gregg <lassitergregg@gmail.com>
2018-11-29 17:43:51 -0600

    add ability to retrieve the first and last record

--- lib/miniorm/selection.rb
+++ lib/miniorm/selection.rb
@@ -57,6 +57,24 @@ module Selection
     init_object_from_row(row)
   end
   
+  def first
+    row = connection.get_first_row <<-SQL
+      SELECT #{columns.join ","} FROM #{table}
+      ORDER BY id ASC LIMIT 1;
+    SQL
+
+    init_object_from_row(row)
+  end
+
+  def last
+    row = connection.get_first_row <<-SQL
+      SELECT #{columns.join ","} FROM #{table}
+      ORDER BY id DESC LIMIT 1;
+    SQL
+
+    init_object_from_row(row)
+  end
+
   private
   def init_object_from_row(row)
     if row
