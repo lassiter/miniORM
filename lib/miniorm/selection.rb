7aafd253e3340b43bd4fc46852f69d3992ef2ae5
Lassiter Gregg <lassitergregg@gmail.com>
2018-11-29 17:33:53 -0600

    setup to retrieve multiple records

--- lib/miniorm/selection.rb
+++ lib/miniorm/selection.rb
@@ -2,7 +2,20 @@
 require 'sqlite3'
 
 module Selection
-  def find(id)
+  def find(*ids)
+    if ids.length == 1
+      find_one(ids.first)
+    else
+      rows = connection.execute <<-SQL
+        SELECT #{columns.join ","} FROM #{table}
+        WHERE id IN (#{ids.join(",")});
+      SQL
+
+      rows_to_array(rows)
+    end
+  end
+
+  def find_one(id)
     row = connection.get_first_row <<-SQL
       SELECT #{columns.join ","} FROM #{table}
       WHERE id = #{id};
@@ -11,6 +24,7 @@ module Selection
     data = Hash[columns.zip(row)]
     new(data)
   end
+
   def find_by(attribute, value)
     row = connection.get_first_row <<-SQL
       SELECT #{columns.join ","} FROM #{table}
@@ -19,5 +33,11 @@ module Selection
 
     data = Hash[columns.zip(row)]
     new(data)
+  end
+  
+  private
+
+  def rows_to_array(rows)
+    rows.map { |row| new(Hash[columns.zip(row)]) }
   end
 end
