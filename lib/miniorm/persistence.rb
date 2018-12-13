33862f92fc8d52fd9186247fb442262e8dd54d32
Lassiter Gregg <lassitergregg@gmail.com>
2018-12-13 12:42:37 -0600

    added class method to delete one item

--- lib/miniorm/persistence.rb
+++ lib/miniorm/persistence.rb
@@ -92,5 +92,14 @@ module Persistence
     def update_all(updates)
       update(nil, updates)
     end
+    
+    def destroy(id)
+      connection.execute <<-SQL
+        DELETE FROM #{table}
+        WHERE id = #{id};
+      SQL
+
+      true
+    end
   end
 end
