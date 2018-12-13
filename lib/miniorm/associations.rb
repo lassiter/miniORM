b4a871b5e8bae27afedfff2fc8f071752c1c2559
Lassiter Gregg <lassitergregg@gmail.com>
2018-12-13 15:11:23 -0600

    added has_one association

--- lib/miniorm/associations.rb
+++ lib/miniorm/associations.rb
@@ -4,6 +4,7 @@ require 'active_support/inflector'
 module Associations
   def has_many(association)
     define_method(association) do
+      binding.pry
       rows = self.class.connection.execute <<-SQL
         SELECT * FROM #{association.to_s.singularize}
         WHERE #{self.class.table}_id = #{self.id}
@@ -17,6 +18,23 @@ module Associations
       end
 
       collection
+    end
+  end
+
+  def has_one(association)
+    define_method(association) do
+      row = self.class.connection.get_first_row <<-SQL
+        SELECT * FROM #{association.to_s.singularize}
+        WHERE #{self.class.table}_id = #{self.id}
+      SQL
+      
+      if row
+        class_name = association.to_s.classify.constantize
+        class_name.new(Hash[class_name.columns.zip(row)])
+      else
+        # Mimicks MiniORM::Collection.new
+        []
+      end
     end
   end
 
